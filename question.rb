0fb535a07cd88bd3e897ed6c5b72ecdca6df6f77
Thitus99 <242421157+Thitusthemaker@users.noreply.github.com>
2026-09-14 10:55:38 +0200

    from lesson 09-14, completed Quiz

--- question.rb
+++ question.rb
@@ -1,21 +1,14 @@
 class Question
+  attr_reader :prompt, :answer
   def initialize(prompt, answer)
+    raise ArgumentError, "prompt must not be empty" if prompt.length == 0
+    raise ArgumentError, "answer must not be empty" if answer.length == 0
     @prompt = prompt
     @answer = answer
   end
-
-  def prompt
-    @prompt
+  def hint
+    "Hint, first letter #{answer[0]}"
   end
-
-  def answer
-    @answer
-  end
-
-  def answer=(new_answer)
-    @answer = new_answer
-  end
-
   def ask
     puts prompt
     gets.chomp
--- quiz.rb
+++ quiz.rb
@@ -1,21 +1,93 @@
 require_relative "question"
+require 'sqlite3'
 
-questions = [
-  Question.new("Vad heter huvudstaden i Norge?", "Oslo"),
-  Question.new("Vilket år släpptes Ruby 1.0?", "1996"),
-  Question.new("Vad svarar 5.class?", "Integer"),
-]
+
+
+class Quiz
+
+  def initialize(db_path)
+    if File.exist?(db_path) != true
+      @db = SQLite3::Database.new(db_path)
+      self.createTable
+      
+    end
+    @db = SQLite3::Database.new(db_path)
+    @questions = []
+    self.loadQuestions
+  end
+
+  def grabQuestions
+    @questions
+  end
+
+  def createQuestion(prompt,answer)
+    raise ArgumentError, "prompt must not be empty" if prompt.length == 0
+    raise ArgumentError, "answer must not be empty" if answer.length == 0
+    @questions << Question.new(prompt,answer)
+    @db.execute('INSERT INTO questions (prompt, answer) VALUES (?, ?)',[prompt,answer])
+   
+    
+  end
+
+  def loadQuestions
+    @questions = []
+    data = @db.execute('SELECT prompt, answer FROM questions')
+    data.each {|question| @questions << Question.new(question[0],question[1])}
+    
+
+  end
+
+  def length
+    @questions.length
+  end
+
+  def createTable
+    @db.execute('DROP TABLE IF EXISTS questions')
+    @db.execute('CREATE TABLE questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            prompt TEXT NOT NULL,
+            answer TEXT NOT NULL)')
+
+  end
+  def seeding #testing
+    @db.execute('INSERT INTO questions (prompt, answer) VALUES ("Vad heter huvudstaden i Norge?", "Oslo")')
+    @db.execute('INSERT INTO questions (prompt, answer) VALUES ("Vilket år släpptes Ruby 1.0?", "1996")')
+    @db.execute('INSERT INTO questions (prompt, answer) VALUES ("Vad svarar 5.class?", "Integer")')
+    @db.execute('INSERT INTO questions (prompt, answer) VALUES ("När lanserades den första iphonen?", "2010")')
+    @db.execute('INSERT INTO questions (prompt, answer) VALUES ("Vad heter sveriges huvudstad?", "Stockholm")')
+    @db.execute('INSERT INTO questions (prompt, answer) VALUES ("Vad heter sveriges bättre stad", "Göteborg")')
+  end
+
+end
+q = Quiz.new('./sqlite.db')
+q.createTable
+q.seeding
+q.createQuestion("en fråga(svaret är idk)", "idk")
+q.loadQuestions
 
 score = 0
 
-questions.each do |q|
-  reply = q.ask
-  if q.correct?(reply)
-    puts "Rätt!"
-    score += 1
-  else
-    puts "Fel. Rätt svar: #{q.answer}"
+q.grabQuestions.each do |q|
+  tries = 0
+  while 1
+    reply = q.ask
+    if q.correct?(reply)
+      puts "Rätt!"
+      if tries == 0
+        score += 1
+      else
+        score += 0.5
+      end
+      break
+    else
+      puts q.hint
+      tries += 1
+      if tries >=2
+        puts "Fel. Rätt svar: #{q.answer}"
+        break
+      end
+    end
   end
 end
 
-puts "#{score} av #{questions.length} rätt."
+puts "#{score} av #{q.length} rätt."
